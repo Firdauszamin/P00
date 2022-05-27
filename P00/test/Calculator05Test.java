02c1b5c1966b204b9d774f782648e62d52d89c1c
21035358 <21035358@10.174.121.204>
2022-05-27 14:34:25 +0800

    First Push to P00 repository

---
+++ P00/test/Calculator05Test.java
@@ -0,0 +1,40 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 21035358, 27 May 2022 11:34:43 am
+ */
+
+/**
+ * @author 21035358
+ *
+ */
+public class Calculator05Test {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
